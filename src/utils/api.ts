1a0dbd2489693f6a8048931d2fd5523bdd79499d
Tobias <1179071115@qq.com>
2026-01-04 16:20:29 +0800

    fix: deploy bugs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -39,13 +39,13 @@ class ApiClient {
     options: RequestInit = {}
   ): Promise<T> {
     const token = localStorage.getItem('token');
-    const headers: HeadersInit = {
-      'Content-Type': 'application/json',
-      ...options.headers,
-    };
+    const headers = new Headers(options.headers);
+    if (!headers.has('Content-Type')) {
+      headers.set('Content-Type', 'application/json');
+    }
 
     if (token) {
-      headers['Authorization'] = `Bearer ${token}`;
+      headers.set('Authorization', `Bearer ${token}`);
     }
 
     const response = await fetch(`${this.baseUrl}${endpoint}`, {
